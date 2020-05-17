0c85e005f25d7ff6969f440ef79071065c740879
Aude Faucheux <aude53@gmail.com>
2020-05-17 11:37:35 +0100

    switch db from sqlite to postgres

--- db/migrate/20190930123438_create_users.rb
+++ db/migrate/20190930123438_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :username
+      t.text :username
       t.integer :credit, default: 100
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_16_185906) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.integer "credit", default: 100
